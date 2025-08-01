5d821f22a8083b835a87b91625ec475b3b935d9e
marcosvandrade <vascorj2006@gmail.com>
2025-08-01 13:04:11 -0300

    create _includes/head.html

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754063758',resources: [
+    cacheName: 'chirpy-1754064198',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
