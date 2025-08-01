76153bddaf9d99990bf8672d683863511b8a71f1
marcosvandrade <vascorj2006@gmail.com>
2025-08-01 13:42:53 -0300

    refactor: remove old colors override file

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754064789',resources: [
+    cacheName: 'chirpy-1754066526',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
