0eae366411843c0b4221e2e8e5d449cc1e8e9434
marcosvandrade <vascorj2006@gmail.com>
2025-08-01 11:51:31 -0300

    update pages.yml to fix github actions

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753897390',resources: [
+    cacheName: 'chirpy-1754059809',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
