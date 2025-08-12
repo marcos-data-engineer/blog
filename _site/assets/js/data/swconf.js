9056e464f94a37ecbfa6e3324c6a0d4e1976ecfc
marcosvandrade <vascorj2006@gmail.com>
2025-08-12 10:22:21 -0300

    updating server

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755004248',resources: [
+    cacheName: 'chirpy-1755004891',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
