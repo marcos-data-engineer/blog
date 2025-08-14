11217c201c6ef319f10a895c02c5d6473a5337bc
marcosvandrade <vascorj2006@gmail.com>
2025-08-14 13:32:00 -0300

    updating logo

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755179939',resources: [
+    cacheName: 'chirpy-1755189062',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
