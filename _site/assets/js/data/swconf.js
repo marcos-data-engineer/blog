0f22d3851a708dbf0b2d63fe7b148fa4601a724d
marcosvandrade <vascorj2006@gmail.com>
2025-08-19 11:57:18 -0300

    include comments and paypal button

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755189062',resources: [
+    cacheName: 'chirpy-1755615150',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
