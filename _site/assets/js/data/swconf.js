73cf12032e1798d5c6f5914c308342557adfb36b
marcosvandrade <vascorj2006@gmail.com>
2025-10-13 11:56:58 -0300

    changing favicon.ico

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760364897',resources: [
+    cacheName: 'chirpy-1760367238',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
