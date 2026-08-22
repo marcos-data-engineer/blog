8d5add2251cff71a11c72ea4cfbe4f7ce57c6683
marcosvandrade <vascorj2006@gmail.com>
2026-08-22 14:27:17 -0300

    Add new tag pages for machine learning and technology

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771850214',resources: [
+    cacheName: 'chirpy-1787419395',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
