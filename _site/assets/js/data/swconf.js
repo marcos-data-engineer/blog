2110a0dc011d6a9462481efc3313c7aa02a08644
marcosvandrade <vascorj2006@gmail.com>
2026-09-14 10:02:27 -0300

    feat: Update site metadata and improve author information for enhanced SEO and branding

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1789153661',resources: [
+    cacheName: 'chirpy-1789390681',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
