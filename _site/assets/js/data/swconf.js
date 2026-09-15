86c7391bd53aa0f4cb84d7e8fdfca5f309b80a46
marcosvandrade <vascorj2006@gmail.com>
2026-09-15 16:00:41 -0300

    feat: Update timestamps and cache name for improved SEO and performance

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1789493547',resources: [
+    cacheName: 'chirpy-1789498626',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
