186dde2a7b1874a424f30f88ce2dfbc33ac28832
marcosvandrade <vascorj2006@gmail.com>
2026-09-16 12:35:22 -0300

    Update cache name and change data mode to light across multiple HTML files; update timestamps in metadata and sitemap

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1789571158',resources: [
+    cacheName: 'chirpy-1789572594',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
