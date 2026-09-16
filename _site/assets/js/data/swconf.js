f8adb50e82411bc817b846b0cf4dde15b011e3c2
marcosvandrade <vascorj2006@gmail.com>
2026-09-16 14:40:13 -0300

    Update timestamps in metadata and sitemap for consistency across multiple files

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1789572594',resources: [
+    cacheName: 'chirpy-1789580080',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
