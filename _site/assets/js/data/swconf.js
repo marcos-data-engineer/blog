a35360956270630ded9d80ba5e939dc099f676d7
marcosvandrade <vascorj2006@gmail.com>
2026-09-16 11:22:05 -0300

    Update cache name and timestamps across multiple files for consistency and accuracy

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1789566881',resources: [
+    cacheName: 'chirpy-1789568051',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
